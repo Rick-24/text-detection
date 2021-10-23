09f6cbe971b4196384143e0d0b9d698f41dd3889
hongwei.zhw <497354870@qq.com>
2021-10-23 21:28:30 +0800

    Add TextDetectioinDTO result model

---
+++ src/main/java/dlut/edu/textdetection/model/model/DetectionModel.java
@@ -0,0 +1,6 @@
+package dlut.edu.textdetection.model.model;/**
+* Created by IntelliJ IDEA.
+* @Author : hongwei.zhw
+* @create 2021/10/23 21:10
+*/public class DetectionModel {
+}
---
+++ src/main/java/dlut/edu/textdetection/model/response/TextDetectionDTO.java
@@ -0,0 +1,6 @@
+package dlut.edu.textdetection.model.response;/**
+* Created by IntelliJ IDEA.
+* @Author : hongwei.zhw
+* @create 2021/10/23 21:05
+*/public class TextDetectionDTO {
+}
